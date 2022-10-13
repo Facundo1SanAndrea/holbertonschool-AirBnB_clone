11c14133de54c0b05ff64dbc33ed65bc8bd69751
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-13 16:28:05 -0300

    making user file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@ import json
 from os import path
 import os
 from models.base_model import BaseModel
+from models.user import User
 
 class FileStorage:
     """serializes instances to a JSON file"""
