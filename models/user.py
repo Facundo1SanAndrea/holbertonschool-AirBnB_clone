fbc3e1913a94784f8c95ddc790997cb7c0daaca5
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-13 16:28:12 -0300

    upladoing user file

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A users class"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """Auser sub class for the base model"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
