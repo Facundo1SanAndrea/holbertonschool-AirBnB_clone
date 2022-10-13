262378b3c0e0214fcaf84e96158cf916f99670cd
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-13 16:48:46 -0300

    uploading review class

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a clas for the review"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """a classs for the inhertance of basemodels"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
