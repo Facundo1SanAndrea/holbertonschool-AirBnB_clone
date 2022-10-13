8925498454973c32f0c65d8f8a12ba2cc03f53d8
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-13 16:49:41 -0300

    state class upload

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a class for the type of state"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """type of state inherits form basemodels"""
+
+    name = ""
