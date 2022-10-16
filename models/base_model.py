9d46d50fd730b3a5103395b2a601737ab662c1b1
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-16 14:42:08 -0300

    fixing elif close of name

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Class for a base modle"""
-import json
 import uuid
 from datetime import datetime
 import models
@@ -13,21 +12,23 @@ class BaseModel:
         forma_t = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    pass
+                if key == "my_number":
+                    self.my_number == value
                 elif key == "id":
                     self.id = value
                 elif key == "created_at":
                     self.created_at = datetime.strptime(value, forma_t)
                 elif key == "updated_at":
                     self.update_at = datetime.strptime(value, forma_t)
+                elif key == 'name':
+                    self.name = value
                 else:
                     setattr(self, key, value)
             else:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.update_at = datetime.now()
-            models.storage.new(self)
+                models.storage.new(self)
 
     def __str__(self):
         """Prints tha name, id and dict"""
