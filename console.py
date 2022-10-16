50ded2850e51ea0b40dc2138484444b032b24467
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-16 15:04:51 -0300

    udating console

--- console.py
+++ console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """program that contains the entry point of the command interpreter"""
-import cmd
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -8,9 +7,10 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.engine import file_storage
+import cmd
 import models
 import json
-from models.engine import file_storage
 
 
 class HBNBCommand(cmd.Cmd):
