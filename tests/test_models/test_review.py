b99eaad8c447aa119fb90616ebc4234dffd4e941
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-17 18:17:08 -0300

    updatin better test for clean

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,42 +1,17 @@
-#!/usr/bin/python3
-
-
+"""Tests for base class review """
 import unittest
-import models
 from models.review import Review
-from models.base_model import BaseModel
-from models import storage
 
 
-class TestReview(unittest.TestCase):
-    """Unittests for the Review class"""
+class Testuser(unittest.TestCase):
+    ''' unittest class for checking review'''
 
-    def test_subclass(self):
-        """checks is Review is a subclass of BaseModel"""
-        self.assertTrue(issubclass(Review, BaseModel))
-
-    def test_instance(self):
-        """Tests if the instances of User are correctly created"""
-        R1 = Review()
-        R2 = Review()
-
-        self.assertNotEqual(R1.id, R2.id)
-        self.assertNotEqual(R1.created_at, R2.created_at)
-        self.assertNotEqual(R1.updated_at, R2.updated_at)
-        self.assertIn(R1, storage.all().values())
-        self.assertIn(R2, storage.all().values())
-        self.assertEqual(Review.place_id, "")
-        self.assertEqual(Review.user_id, "")
-        self.assertEqual(Review.text, "")
-
-
-    def test_attr(self):
-        """Tests is an attribute of an instance is created"""
-        my_review = Review()
-		
-        self.assertEqual(my_review.place_id, "")
-        self.assertEqual(my_review.user_id, "")
-        self.assertEqual(my_review.text, "")
+    def test_create_place(self):
+        ''' creating review class '''
+        review1 = Review()
+        self.assertEqual(review1.place_id, "")
+        self.assertEqual(review1.user_id, "")
+        self.assertEqual(review1.text, "")
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,38 +1,16 @@
-#!/usr/bin/python3
 
-
+"""Tests for base class state """
 import unittest
-import models
 from models.state import State
-from models import storage
-from datetime import datetime
-from models.base_model import BaseModel
 
 
-class TestState(unittest.TestCase):
-    """Unittests for the State class"""
+class Testuser(unittest.TestCase):
+    ''' unittest class for checking state'''
 
-    def test_subclass(self):
-        """checks is State is a subclass of BaseModel"""
-        self.assertTrue(issubclass(State, BaseModel))
-
-    def test_instance(self):
-        """Tests if the instances of User are correctly created"""
-        S1 = State()
-        S2 = State()
-
-        self.assertNotEqual(S1.id, S2.id)
-        self.assertNotEqual(S1.created_at, S2.created_at)
-        self.assertNotEqual(S1.updated_at, S2.updated_at)
-        self.assertIn(S1, storage.all().values())
-        self.assertIn(S2, storage.all().values())
-        self.assertEqual(State.name, "")
-
-
-    def test_attr(self):
-        """Tests is an attribute of an instance is created"""
-        my_state = State()
-        self.assertEqual(my_state.name, "")
+    def test_create_state(self):
+        ''' creating state class '''
+        state1 = State()
+        self.assertEqual(state1.name, "")
 
 if __name__ == '__main__':
     unittest.main()
