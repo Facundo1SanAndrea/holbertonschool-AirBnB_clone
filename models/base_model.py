6cbf91370d54c281bfccba6082e7860a3b626e95
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-17 12:45:48 -0300

    fixing return json file id error

--- models/base_model.py
+++ models/base_model.py
@@ -7,44 +7,44 @@ import models
 
 
 class BaseModel:
-    """A class of a base modle"""
+    """A class of a base model """
 
     def __init__(self, *args, **kwargs):
-        """public instance attri"""
+        """public instance attribute"""
         forma_t = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == '__class__':
                     pass
-                elif key == "id":
+                elif key == 'id':
                     self.id = value
-                elif key == "created_at":
+                elif key == 'created_at':
                     self.created_at = datetime.strptime(value, forma_t)
-                elif key == "updated_at":
-                    self.update_at = datetime.strptime(value, forma_t)
+                elif key == 'updatedd_at':
+                    self.updatedd_at = datetime.strptime(value, forma_t)
                 elif key == 'name':
                     self.name = value
                 else:
                     setattr(self, key, value)
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.update_at = datetime.now()
-            models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updatedd_at = datetime.now()
+        models.storage.new(self)
 
     def __str__(self):
         """Prints tha name, id and dict"""
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return (f"[{self.__class__.__name__}] ({self.id} {self.__dict__}")
 
     def save(self):
-        """updates the public instance attribute"""
-        self.update_at = datetime.now()
+        """updateds the public instance attribute"""
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["updatedd_at"] = self.updatedd_at.isoformat()
         new_dict['__class__'] = self.__class__.__name__
         return new_dict
